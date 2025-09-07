0e2ab4f8d36ba3216ac1025efac29230316920b8
asapkobby4 <asapkoby4@gmail.com>
2025-09-07 19:37:59 -0400

    coffee shop project

---
+++ Coffee-Shop/src/pages/Inventory.jsx
@@ -0,0 +1,8 @@
+export default function Inventory() {
+    return (
+    <div>
+    <h2 className="text-2xl font-bold mb-4">Inventory</h2>
+    <p>Mock inventory list here.</p>
+    </div>
+    );
+    }
